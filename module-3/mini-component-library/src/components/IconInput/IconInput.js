81fc695b6ffb8dbfabd13778efc7f6572e39f502
Joe Dravarol <joedravarol@gmail.com>
2023-11-20 14:45:56 +0000

    feat: Build text input with icon

--- module-3/mini-component-library/src/components/IconInput/IconInput.js
+++ module-3/mini-component-library/src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    padding: 4,
+    paddingLeft: 8,
+    iconSize: 12,
+  },
+  large: {
+    padding: 8,
+    paddingLeft: 12,
+    iconSize: 16,
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,78 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper>
+      <IconWrapper
+        style={{
+          '--size': styles.iconSize + 'px'
+        }}
+      >
+        <VisuallyHidden htmlFor="icon">
+          {label}
+        </VisuallyHidden>
+        <Icon 
+          id={icon} 
+          size={styles.iconSize}
+        />
+      </IconWrapper>
+      <NativeInput
+        style={{
+          '--padding': styles.padding + 'px',
+          '--paddingLeft': styles.iconSize + styles.paddingLeft + 'px',
+          '--width': width + 'px',
+        }}
+        placeholder={placeholder}
+        iconSize={styles.iconSize}
+      />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.span`
+  position: relative;
+`;
+
+const IconWrapper = styled.span`
+  width: var(--size);
+  height: var(--size);
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  margin: auto;
+  pointer-events: none;
+`;
+
+const NativeInput = styled.input`
+  padding: var(--padding);
+  width: var(--width);
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: var(--paddingLeft);
+  font-weight: 700;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    border-width: 2px;
+    color: ${COLORS.black}
+  }
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+    font-size: 0.875rem;
+  }
+
+  & > svg {
+    color: inherit;
+  }
+`;
 
 export default IconInput;
